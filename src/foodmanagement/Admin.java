54e5fe94fbc6cd17eb5e32819bbb8187db12a558
Ravishankar08 <ravigurunatha2001@gmail.com>
2022-06-22 17:03:18 +0530

    Adding Missed Files

---
+++ src/foodmanagement/Admin.java
@@ -0,0 +1,10 @@
+
+package foodmanagement;
+
+import java.util.ArrayList;
+
+
+public class Admin {
+    static ArrayList<Hotel> hotel_list = new ArrayList<Hotel>();
+    static ArrayList<User> user_list = new ArrayList<User>();
+}
---
+++ src/foodmanagement/Food.java
@@ -0,0 +1,15 @@
+
+package foodmanagement;
+
+import java.util.*;
+
+class Food{
+   
+    HashMap<String, Integer> menu_aval = new HashMap<>();
+    HashMap<String, Integer> menu_cost = new HashMap<>();
+}
+class user_order_class {
+
+    protected String food, Hotel;
+    protected int quantity, amount;
+}
